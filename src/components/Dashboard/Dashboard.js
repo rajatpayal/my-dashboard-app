f9ac0962b7c37aae27209c0c163db3eb50f60cfb
rajatpayal <68436197+rajatpayal@users.noreply.github.com>
2024-07-04 01:42:38 +0530

    isue

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -1,26 +1,3 @@
-<<<<<<< Updated upstream
-import React from 'react';
-import { Container, Row, Col } from 'react-bootstrap';
-import AttendanceCard from './AttendenceCard/AttendanceCard';
-import LeaveRequestsCard from './LeaveRequestsCard/LeaveRequestsCard';
-import TimeTrackerCard from './TimeTrackerCard/TimeTrackerCard';
-import AnnouncementsCard from './AnnouncementsCard/AnnouncementsCard';
-import UpcomingHolidaysCard from './UpcomingHolidaysCard/UpcomingHolidaysCard';
-
-const Dashboard = () => {
-  return (
-    <Container fluid className="p-4">
-      <Row>
-        <Col md={4}><AttendanceCard /></Col>
-        <Col md={4}><LeaveRequestsCard /></Col>
-        <Col md={4}><TimeTrackerCard /></Col>
-      </Row>
-      <Row>
-        <Col md={6}><AnnouncementsCard /></Col>
-        <Col md={6}><UpcomingHolidaysCard /></Col>
-      </Row>
-    </Container>
-=======
 import React from "react";
 import "./Dashboard.css";
 
@@ -81,7 +58,6 @@ const Dashboard = () => {
         </div>
       </div>
     </>
->>>>>>> Stashed changes
   );
 };
 
--- src/components/sidebar/Sidebar.js
+++ src/components/sidebar/Sidebar.js
@@ -1,14 +1,8 @@
 import React from "react";
-<<<<<<< Updated upstream
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faHome, faClock, faUser, faCalendar, faKey, faSignOutAlt } from '@fortawesome/free-solid-svg-icons';
-import "./Sidebar.css";
-=======
 import { Home, Calendar, Lock, LogOut, FileText } from "react-feather";
 import "./Sidebar.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faUserTimes } from "@fortawesome/free-solid-svg-icons";
->>>>>>> Stashed changes
+import { faUser } from "@fortawesome/free-solid-svg-icons";
 
 const Sidebar = ({ isOpen, setActiveComponent }) => {
   const handleMenuItemClick = (component) => {
@@ -18,9 +12,6 @@ const Sidebar = ({ isOpen, setActiveComponent }) => {
   return (
     <div className={`sidebar ${isOpen ? "" : "closed"}`}>
       <div className="sidebar-header">
-<<<<<<< Updated upstream
-        {isOpen && <h3>106. Sachin kamboj</h3>}
-=======
         <div>
           {isOpen && (
             <span className="user-descripition">106. Sachin kamboj</span>
@@ -31,21 +22,9 @@ const Sidebar = ({ isOpen, setActiveComponent }) => {
             <span className="user-descripition">Frontend Developer</span>
           )}
         </div>
->>>>>>> Stashed changes
       </div>
       <ul className="sidebar-menu">
         <li onClick={() => handleMenuItemClick("Dashboard")}>
-<<<<<<< Updated upstream
-          <FontAwesomeIcon icon={faHome} />
-          {isOpen && <span>Dashboard</span>}
-        </li>
-        <li onClick={() => handleMenuItemClick("Attendance")}>
-          <FontAwesomeIcon icon={faClock} />
-          {isOpen && <span>Attendance</span>}
-        </li>
-        <li onClick={() => handleMenuItemClick("Time Tracker")}>
-          <FontAwesomeIcon icon={faClock} />
-=======
           <FontAwesomeIcon icon={Home} />
           <Home />
           {isOpen && <span>Dashboard</span>}
@@ -58,7 +37,6 @@ const Sidebar = ({ isOpen, setActiveComponent }) => {
         <li onClick={() => handleMenuItemClick("Time Tracker")}>
           <FontAwesomeIcon icon={FileText} />
           <FileText />
->>>>>>> Stashed changes
           {isOpen && <span>Time Tracker</span>}
         </li>
         <li onClick={() => handleMenuItemClick("Leave")}>
@@ -66,17 +44,6 @@ const Sidebar = ({ isOpen, setActiveComponent }) => {
           {isOpen && <span>Leave</span>}
         </li>
         <li onClick={() => handleMenuItemClick("Holidays")}>
-<<<<<<< Updated upstream
-          <FontAwesomeIcon icon={faCalendar} />
-          {isOpen && <span>Holidays</span>}
-        </li>
-        <li onClick={() => handleMenuItemClick("ChangePassword")}>
-          <FontAwesomeIcon icon={faKey} />
-          {isOpen && <span>Change Password</span>}
-        </li>
-        <li onClick={() => handleMenuItemClick("Logout")}>
-          <FontAwesomeIcon icon={faSignOutAlt} />
-=======
           <FontAwesomeIcon icon={Calendar} />
           <Calendar />
           {isOpen && <span>Holidays</span>}
@@ -89,7 +56,6 @@ const Sidebar = ({ isOpen, setActiveComponent }) => {
         <li onClick={() => handleMenuItemClick("Logout")}>
           <FontAwesomeIcon icon={LogOut} />
           <LogOut />
->>>>>>> Stashed changes
           {isOpen && <span>Log Out</span>}
         </li>
       </ul>
