d5ce95c3f338532ee5a603b192b25391b9926810
rajatpayal <68436197+rajatpayal@users.noreply.github.com>
2024-07-04 16:42:14 +0530

    update app.js

--- src/App.js
+++ src/App.js
@@ -16,6 +16,30 @@ import TimeTrackerMain from './components/timeTracker/timeTrackerMain';
 const App = () => {
   const [activeComponent, setActiveComponent] = useState("Dashboard");
   const [isSidebarOpen, setIsSidebarOpen] = useState(true);
+<<<<<<< Updated upstream
+=======
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const [userId, setUserId] = useState(null);
+
+  const fetchUserId = async () => {
+    const token = localStorage.getItem('token');
+    if (token) {
+      setIsAuthenticated(true);
+      try {
+        const response = await axios.get('http://localhost:5000/api/auth/protected', { headers: { 'x-access-token': token } });
+        setUserId(response.data.userId);
+      } catch (error) {
+        console.error('AxiosError:', error);
+      }
+    } else {
+      setIsAuthenticated(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchUserId();
+  }, []);
+>>>>>>> Stashed changes
 
   const toggleSidebar = () => {
     setIsSidebarOpen(!isSidebarOpen);
@@ -40,6 +64,46 @@ const App = () => {
     }
   };
 
+<<<<<<< Updated upstream
+=======
+  const AppContent = () => {
+    const location = useLocation();
+
+    useEffect(() => {
+      if (!isAuthenticated && location.pathname !== '/login') {
+        window.location.href = '/login';
+      }
+    }, [location.pathname]);
+
+    return (
+      <Routes>
+        <Route
+          path="/login"
+          element={!isAuthenticated ? <Login onLogin={handleLogin} /> : <Navigate to="/dashboard" />}
+        />
+        <Route
+          path="/dashboard"
+          element={isAuthenticated ? (
+            <div className={`app ${isSidebarOpen ? "sidebar-open" : "sidebar-closed"}`}>
+              <Sidebar isOpen={isSidebarOpen} setActiveComponent={setActiveComponent} />
+              <div className="main-content">
+                <NavbarComponent toggleSidebar={toggleSidebar} sidebarOpen={isSidebarOpen} />
+                {renderComponent()}
+              </div>
+            </div>
+          ) : (
+            <Navigate to="/login" />
+          )}
+        />
+        <Route
+          path="/"
+          element={<Navigate to={isAuthenticated ? "/dashboard" : "/login"} />}
+        />
+      </Routes>
+    );
+  };
+
+>>>>>>> Stashed changes
   return (
     <div className="app">
     <Sidebar isOpen={isSidebarOpen} setActiveComponent={setActiveComponent} />
